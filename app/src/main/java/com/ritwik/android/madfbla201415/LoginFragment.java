b35d346e43727eea20f67516dd9bf97cf1b0f5dd
Soham Pardeshi <sohampardeshi@gmail.com>
2014-12-30 19:34:26 -0800

    User Authentication for Login Improved

--- app/src/main/java/com/ritwik/android/madfbla201415/LoginFragment.java
+++ app/src/main/java/com/ritwik/android/madfbla201415/LoginFragment.java
@@ -1,5 +1,7 @@
 package com.ritwik.android.madfbla201415;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -20,6 +22,7 @@ public class LoginFragment extends Fragment {
     private TextView mSignUpButtonView;
     private EditText mLoginField, mPassField;
     private Firebase ref;
+    private boolean isSuccess = false;
 
     //Constants
     private static final String URL_FIREBASE = "https://chronology.firebaseio.com";
@@ -32,72 +35,84 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
-
-        mLoginField = (EditText) rootView.findViewById(R.id.login_username);
-        mPassField = (EditText) rootView.findViewById(R.id.login_password);
-        mSignInButton = (Button) rootView.findViewById(R.id.sign_in_button);
-        mSignUpButtonView = (TextView) rootView.findViewById(R.id.sign_up_buttonview);
-
+        final View rootView = inflater.inflate(R.layout.fragment_login, container, false);
         Firebase.setAndroidContext(getActivity());
         ref = new Firebase(URL_FIREBASE);
 
-        //creating anonymous inner classes for click events
-        mSignInButton.setOnClickListener(new View.OnClickListener() {
-
+        ref.addAuthStateListener(new Firebase.AuthStateListener() {
             @Override
-            public void onClick(View v) {
-
-                ref.authWithPassword(mLoginField.getText().toString(), mPassField.getText().toString() , new Firebase.AuthResultHandler() {
-                    @Override
-                    public void onAuthenticated(AuthData authData) {
-                       //Auth Successful
-                       //TODO: Move to HomeScreen
-                    }
-                    @Override
-                    public void onAuthenticationError(FirebaseError firebaseError) {
-                       //Auth Error
-                       //TODO: Display somehow :  firebaseError.getMessage();
-                    }
-                });
+            public void onAuthStateChanged(AuthData authData) {
+                //if user is authenticated
+                if (authData != null) {
+                    Intent homepageIntent = new Intent(getActivity(), HomepageActivity.class);
+                    startActivity(homepageIntent);
+                //if user is not authenticated, display login
+                } else {
+                    mLoginField = (EditText) rootView.findViewById(R.id.login_username);
+                    mPassField = (EditText) rootView.findViewById(R.id.login_password);
+                    mSignInButton = (Button) rootView.findViewById(R.id.sign_in_button);
+                    mSignUpButtonView = (TextView) rootView.findViewById(R.id.sign_up_buttonview);
 
 
-                //Read about Explicit Intents at
-                // http://developer.android.com/guide/components/intents-filters.html
-                //used to start activities (like this) and send data across activities
 
-                Intent homepageIntent = new Intent(getActivity(), HomepageActivity.class);
-                startActivity(homepageIntent);
+                    //creating anonymous inner classes for click events
+                    mSignInButton.setOnClickListener(new View.OnClickListener() {
 
+                        @Override
+                        public void onClick(View v) {
+
+                            ref.authWithPassword(mLoginField.getText().toString(), mPassField.getText().toString() , new Firebase.AuthResultHandler() {
+                                @Override
+                                public void onAuthenticated(AuthData authData) {
+                                    //Read about Explicit Intents at
+                                    //http://developer.android.com/guide/components/intents-filters.html
+                                    //used to start activities (like this) and send data across activities
+                                    Intent homepageIntent = new Intent(getActivity(), HomepageActivity.class);
+                                    startActivity(homepageIntent);
+                                }
+                                @Override
+                                public void onAuthenticationError(FirebaseError firebaseError) {
+                                    //Create alert with firebaseError.getMessage()
+                                    AlertDialog.Builder builder1 = new AlertDialog.Builder(getActivity());
+                                    builder1.setMessage(firebaseError.getMessage());
+                                    builder1.setCancelable(true);
+                                    builder1.setPositiveButton("Ok",
+                                            new DialogInterface.OnClickListener() {
+                                                public void onClick(DialogInterface dialog, int id) {
+                                                    dialog.cancel();
+                                                }
+                                            });
+                                    AlertDialog alert11 = builder1.create();
+                                    alert11.show();
+                                }
+                            });
+                        }
+                    });
+
+                    mSignUpButtonView.setOnClickListener(new View.OnClickListener() {
+
+                        @Override
+                        public void onClick(View v) {
+                            //Switching fragments (View) inside LoginActivity using support fragment manager
+                            //replacing fragment container (the activity's root view) with new SignupFragment
+                            //addToBackStack is called so that user can press back to go back to LoginFragment
+                            getActivity().getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.fragment_main_container, new SignupFragment())
+                                    .addToBackStack(null).commit();
+                        }
+                    });
+                }
             }
-
         });
 
-        mSignUpButtonView.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                //Switching fragments (View) inside LoginActivity using support fragment manager
-                //replacing fragment container (the activity's root view) with new SignupFragment
-                //addToBackStack is called so that user can press back to go back to LoginFragment
-
-                getActivity().getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.fragment_main_container, new SignupFragment())
-                        .addToBackStack(null).commit();
-
-            }
-
-        });
-
-        /*
-        For reference
-        Testing adding values with distinct key pairs
-        Map<String, Object> mapp = new HashMap<String, Object>();
-        mapp.put("runtimeMessage3", "more data");
-        mapp.put("runtimeMessage4", "more data");
-        ref.child("announcements").child("current").updateChildren(mapp);
-        */
+         /*
+            For reference
+            Testing adding values with distinct key pairs
+            Map<String, Object> mapp = new HashMap<String, Object>();
+            mapp.put("runtimeMessage3", "more data");
+            mapp.put("runtimeMessage4", "more data");
+            ref.child("announcements").child("current").updateChildren(mapp);
+         */
 
         return rootView;
     }
