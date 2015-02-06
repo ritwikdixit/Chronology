98243b61245b4a8f7e7a342ec73b14031e3428b4
Soham Pardeshi <sohampardeshi@gmail.com>
2015-02-06 00:03:55 -0800

    Calendar Improvements

--- app/src/main/java/com/ritwik/android/madfbla201415/ImprovedCalendarActivity.java
+++ app/src/main/java/com/ritwik/android/madfbla201415/ImprovedCalendarActivity.java
@@ -1,6 +1,8 @@
 package com.ritwik.android.madfbla201415;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
@@ -12,6 +14,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -30,6 +33,7 @@ import hirondelle.date4j.DateTime;
 public class ImprovedCalendarActivity extends ActionBarActivity {
     private ListView mEventsList;
     private EventListItemAdapter adapter;
+    private Context mContext = this;
 
     private ArrayList<EventItem> events;
     private ArrayList<EventItem> filteredEvents;
@@ -45,8 +49,6 @@ public class ImprovedCalendarActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
     private SearchView mSearch;
-
-
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,53 +83,98 @@ public class ImprovedCalendarActivity extends ActionBarActivity {
 
         caldroidFragment = new CaldroidFragment();
         Bundle args = new Bundle();
-        Calendar cal = Calendar.getInstance();
+        final Calendar cal = Calendar.getInstance();
         args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
         args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
         args.putBoolean(CaldroidFragment.SIX_WEEKS_IN_CALENDAR, false);
         caldroidFragment.setArguments(args);
-        for(EventItem ei : events){
-            ArrayList<Date> ald = between(ei.dateStart(), ei.dateEnd());
+
+
+
+        for(EventItem ei : HomepageFragment.getEvents()){
+            ArrayList<Date> ald = between(ei.dateStart(false), ei.dateEnd(false));
             for(Date d : ald) {
-                caldroidFragment.setBackgroundResourceForDate(R.color.caldroid_holo_blue_dark, d);
-                caldroidFragment.setTextColorForDate(R.color.almost_white, d);
+                filterEvents(d);
+                if (!filteredEvents.isEmpty()) {
+                    caldroidFragment.setBackgroundResourceForDate(R.color.calendar_orange, d);
+                    caldroidFragment.setTextColorForDate(R.color.almost_white, d);
+                }
             }
         }
-        final AtomicReference<Date> lastDate = new AtomicReference<Date>(Calendar.getInstance().getTime());
+
         final CaldroidListener listener = new CaldroidListener() {
             public void onSelectDate(Date date, View view) {
-                caldroidFragment.setBackgroundResourceForDate(R.color.caldroid_white, lastDate.get());
-                caldroidFragment.setTextColorForDate(R.color.caldroid_black, lastDate.get());
-
-                lastDate.set(date);
-
-                caldroidFragment.setBackgroundResourceForDate(R.color.caldroid_holo_blue_light, date);
-                caldroidFragment.setTextColorForDate(R.color.almost_white, date);
-
-                caldroidFragment.refreshView();
-                filterEvents(date, events);
+                filterEvents(date);
                 setText(date);
                 mEventsList.setAdapter(adapter);
+
+                caldroidFragment.refreshView();
             }
         };
         caldroidFragment.setCaldroidListener(listener);
+
+
         caldroidFragment.refreshView();
         FragmentTransaction t = getSupportFragmentManager().beginTransaction();
         t.replace(R.id.calendar_chronology, caldroidFragment);
+        filterEvents(new Date());
         t.commit();
+
+        addEventClicker();
     }
 
-    private void filterEvents(Date date, ArrayList<EventItem> allEvents) {
+    private void addEventClicker() {
+        mEventsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //Putting data for detail activity
+                Intent detailIntent = new Intent(mContext, DetailActivity.class);
+                detailIntent.putExtra(Intent.EXTRA_TEXT,
+                        filteredEvents.get(position).getNumber());
+                detailIntent.putExtra(HomepageFragment.TITLE_KEY,
+                        filteredEvents.get(position).getmTitle());
+                detailIntent.putExtra(HomepageFragment.START_DATE_KEY,
+                        events.get(position).getmStartDate());
+                detailIntent.putExtra(HomepageFragment.END_DATE_KEY,
+                        events.get(position).getmEndDate());
+                detailIntent.putExtra(HomepageFragment.START_TIME_KEY,
+                        filteredEvents.get(position).getmStartTime());
+                detailIntent.putExtra(HomepageFragment.END_TIME_KEY,
+                        filteredEvents.get(position).getmEndTime());
+                detailIntent.putExtra(HomepageFragment.LOCATION_KEY,
+                        filteredEvents.get(position).getmLocation());
+                detailIntent.putExtra(HomepageFragment.DETAILS_KEY,
+                        filteredEvents.get(position).getmDetails());
+                detailIntent.putExtra(HomepageFragment.URL_KEY,
+                        filteredEvents.get(position).getmUrl());
+                detailIntent.putExtra(HomepageFragment.CONTACT_INFO_KEY,
+                        filteredEvents.get(position).getmContactInfo());
+                detailIntent.putExtra(HomepageFragment.CATEGORY_KEY,
+                        filteredEvents.get(position).getCategory());
+                detailIntent.putExtra(HomepageFragment.RSVP_KEY,
+                        filteredEvents.get(position).isAttending());
+
+                startActivity(detailIntent);
+                ImprovedCalendarActivity.this.overridePendingTransition(
+                        R.anim.right_to_left, R.anim.neg_right_left);
+
+            }
+        });
+
+    }
+
+    private void filterEvents(Date date) {
         filteredEvents.clear();
 
-        for (EventItem thisEvent : allEvents) {
-            if (thisEvent.dateStart().compareTo(date) < 1
-                    && thisEvent.dateEnd().compareTo(date) > -1) {
+        for (EventItem thisEvent : HomepageFragment.getEvents()) {
+            if (thisEvent.dateStart(true).compareTo(date) < 1
+                    && thisEvent.dateEnd(true).compareTo(date) > -1) {
                 filteredEvents.add(thisEvent);
             }
         }
     }
-
     private void initDrawer() {
         mDrawerLayout = (DrawerLayout) findViewById(R.id.navigation_drawer);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
@@ -193,7 +240,7 @@ public class ImprovedCalendarActivity extends ActionBarActivity {
             mDataText.setText(filteredEvents.size() + " Events on " + EventItem.formatDate(todayDate));
         }
     }
-    private ArrayList<Date> between(Date start, Date end){
+    private ArrayList<Date> between(Date start, Date end) {
         ArrayList<Date> dates = new ArrayList<Date>();
         Calendar calendar = new GregorianCalendar();
         calendar.setTime(start);
@@ -204,5 +251,13 @@ public class ImprovedCalendarActivity extends ActionBarActivity {
             calendar.add(Calendar.DATE, 1);
         }
         return dates;
+    }
+    Calendar cal1 = Calendar.getInstance();
+    Calendar cal2 = Calendar.getInstance();
+    public boolean equals(Date a, Date b){
+        cal1.setTime(a);
+        cal2.setTime(b);
+        return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
+                cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR);
     }
 }
