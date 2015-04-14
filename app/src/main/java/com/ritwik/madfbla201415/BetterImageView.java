c22c7304aefba2bbebad2e8f0a63746e5a63f7ec
jx2zhou <joshua.j.zhou@gmail.com>
2015-04-13 22:31:56 -0700

    Package rename; test

---
+++ app/src/main/java/com/ritwik/madfbla201415/BetterImageView.java
@@ -0,0 +1,36 @@
+package com.ritwik.madfbla201415;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by Ritwik on 1/4/15.
+ */
+
+
+//class to scale images properly in the detail activity
+
+public class BetterImageView extends ImageView {
+
+    public BetterImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        final Drawable d = getDrawable();
+        if (d != null) {
+
+            final int width = MeasureSpec.getSize(widthMeasureSpec);
+            final int height = (int) (Math.ceil(width *
+                    (float) d.getIntrinsicHeight() / d.getIntrinsicWidth()));
+            this.setMeasuredDimension(width, height);
+
+        } else {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+}
---
+++ app/src/main/java/com/ritwik/madfbla201415/SwipeListener.java
@@ -0,0 +1,35 @@
+package com.ritwik.madfbla201415;
+
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+
+//listens for a left to right swipe
+public class SwipeListener extends GestureDetector.SimpleOnGestureListener {
+
+    public static final String TAG = "Swipes";
+    private boolean swiped = false;
+
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+
+        Log.v(TAG, "Swipe Works " + velocityX + " " + velocityY);
+
+        if (velocityX > 1500 && Math.abs(velocityY) < 2000) {
+            swiped = true;
+        }
+
+        return true;
+    }
+
+    public boolean hasSwiped() {
+        return swiped;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return true;
+    }
+}
